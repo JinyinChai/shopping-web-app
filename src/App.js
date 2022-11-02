0666ad3cdf6f0100dcfad39a548cb28c3cac3e35
Jinyin Chai <cjy990324>
2022-11-02 15:05:35 -0700

    basic pages and UI design

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import Home from "./pages/Home";
-import Navbar from "./components/Navbar";
+import ProductList from "./pages/ProductList";
+import Product from "./pages/Product";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Cart from "./pages/Cart";
 
 const App = () => {
-  return <Navbar/>;
+  return <Cart/>;
 };
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,71 @@
+import {lightBlue} from "@material-ui/core/colors";
+
+export const sliderItems = [
+    {
+        id: 1,
+        img: require("./images/home_img2.jpeg"),
+        title: "ThanksGiving Sale",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "E1FFFE",
+    },
+    {
+        id: 2,
+        img: require("./images/home_img1.jpeg"),
+        title: "Autumn Sale",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "fcf1ed",
+    }
+];
+
+export const categories = [
+    {
+        id: 1,
+        img: "https://images.pexels.com/photos/5886041/pexels-photo-5886041.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+        title: "SHIRT STYLE!",
+    },
+    {
+        id: 2,
+        img: "https://images.pexels.com/photos/2983464/pexels-photo-2983464.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+        title: "LOUNGEWEAR LOVE",
+    },
+    {
+        id: 3,
+        img: "https://images.pexels.com/photos/5480696/pexels-photo-5480696.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+        title: "LIGHT JACKETS",
+    },
+];
+
+export const popularProducts = [
+    {
+        id:1,
+        img:"https://d3o2e4jr3mxnm3.cloudfront.net/Mens-Jake-Guitar-Vintage-Crusher-Tee_68382_1_lg.png",
+    },
+    {
+        id:2,
+        img:"https://image.uniqlo.com/UQ/ST3/WesternCommon/imagesgoods/448474/item/goods_42_448474.jpg?width=750",
+    },
+    {
+        id:3,
+        img:"https://www.prada.com/content/dam/pradanux_products/U/UCS/UCS319/1YOTF010O/UCS319_1YOT_F010O_S_182_SLF.png",
+    },
+    {
+        id:4,
+        img:"https://image.uniqlo.com/UQ/ST3/WesternCommon/imagesgoods/445051/item/goods_55_445051.jpg?width=750",
+    },
+    {
+        id:5,
+        img:"https://images.ctfassets.net/5gvckmvm9289/3BlDoZxSSjqAvv1jBJP7TH/65f9a95484117730ace42abf64e89572/Noissue-x-Creatsy-Tote-Bag-Mockup-Bundle-_4_-2.png",
+    },
+    {
+        id:6,
+        img:"https://d3o2e4jr3mxnm3.cloudfront.net/Rocket-Vintage-Chill-Cap_66374_1_lg.png",
+    },
+    {
+        id:7,
+        img:"https://image.uniqlo.com/UQ/ST3/WesternCommon/imagesgoods/442147/item/goods_57_442147.jpg?width=750",
+    },
+    {
+        id:8,
+        img:"https://www.pngarts.com/files/3/Women-Jacket-PNG-High-Quality-Image.png",
+    },
+]
