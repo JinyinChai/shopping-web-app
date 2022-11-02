a0c9732ad5804df7e91c60881e432bb8fb1ff14a
Jinyin Chai <cjy990324>
2022-11-02 15:49:24 -0700

    basic pages and UI design

--- src/App.js
+++ src/App.js
@@ -4,9 +4,20 @@ import Product from "./pages/Product";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Cart from "./pages/Cart";
+import {Route, Router, Routes} from "react-router";
+import {BrowserRouter} from "react-router-dom";
+
 
 const App = () => {
-  return <Cart/>;
+  return (
+      <BrowserRouter>
+          <Routes>
+              <Route path="/"
+                     element={<Home/>}/>
+              <Route path="/products" element={<ProductList/>}/>
+          </Routes>
+      </BrowserRouter>
+  );
 };
 
 export default App;
