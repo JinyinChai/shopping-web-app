71f6f193660887bad50ae9e0f558a7aeafa713b1
Jinyin Chai <cjy990324>
2022-12-05 14:27:51 -0800

    ecommerce update v2

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -5,8 +5,8 @@ import axios from "axios";
 const user = JSON.parse(localStorage.getItem("persist:root"))?.user;
 const currentUser = user && JSON.parse(user).currentUser;
 const TOKEN = currentUser?.accessToken;
-const BASE_URL = "http://localhost:5001/api/"
-//const BASE_URL = process.env.REACT_APP_API_BASE;
+// const BASE_URL = "http://localhost:5001/api/"
+const BASE_URL = process.env.REACT_APP_API_BASE;
 
 export const userRequest = axios.create({
     baseURL: BASE_URL,
